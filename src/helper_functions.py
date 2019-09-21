18a83e3b7675abe15f6d16eb06f0c6983b2c4e8e
Dmitry Selin <des3358@rit.edu>
2019-09-21 00:39:29 -0400

    Moved total_words into helper_functions Provided proper documentation

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,10 +1,37 @@
+"""
+CSAPX Project: Helper Functions
+
+A module with snippets of code that are used multiple
+times throughout Project 1
+
+Author: Dmitry Selin
+
+Date Created: September 19, 2019
+Last Modified: September 21, 2019
+"""
+
 import collections
 import sys
+from typing import List
 
 
 def read_file(filename: str) -> dict:
+    """
+    Reads a file (filename) and parses the lines of the .csv file
+    into a dictionary
+
+    :param filename: a string with the the name of a csv file in the form data/name-of-file
+    :return: a dictionary with words as keys and lists with tuples of years and occurrences as elements
+
+    Pre: the file with the name filename must exist be formatted as a csv file type
+    Post: the returned dictionary will contain one or more elements of listings as values for each key
+    """
+
+    # Initializes file as a blank string
     file = ""
 
+    # Attempts to open the user-given file, if the file is not found, an exception is
+    # thrown, a standard error is displayed, and the program is terminated
     try:
         file = open("../" + filename)
 
@@ -12,16 +39,47 @@ def read_file(filename: str) -> dict:
         sys.stderr.write("Error: " + filename + " does not exist!\n")
         exit(1)
 
+    # Initializes Word (for better readability) and occurrences (the dictionary that will be returned)
     Word = collections.namedtuple("Occurrence", ("word", "year", "usage"))
     occurrences = {}
 
+    # Iterates through each line in file
     for line in file:
-        components = line.strip().split(',')
-        entry = Word(components[0].strip(), components[1].strip(), components[2].strip())
 
+        # Splits line into its 3 components (parses by commas) and strips any excess whitespace
+        components = line.strip().split(',')
+
+        # Creates a Word tuple, entry, with the 3 components as parameters (component[0] is set to lowercase for consistency)
+        # components[0] = word, components[1] = year, components[2] = number of occurrences
+        entry = Word(components[0].strip().lower(), components[1].strip(), components[2].strip())
+
+        # If the word is already a key, append its year and usage to its value list as a tuple,
+        # otherwise, simply add a new entry to occurrences
         if entry.word in occurrences:
             occurrences[entry.word].append((entry.year, entry.usage))
         else:
             occurrences[entry.word] = [(entry.year, entry.usage)]
 
     return occurrences
+
+
+def total_words(target_word: List) -> int:
+    """
+    Adds the number of occurrences a particular word has and returns the number
+
+    :param target_word: a list of two-element tuples that contain the year and number of occurrences of a particular word
+    :return: int (word_count)
+
+    Pre: target_word must contain two-element tuples, where the second element in each tuple is the usage of the word
+    Post: None
+    """
+
+    # Initializes word_count
+    word_count = 0
+
+    # Iterates through each listing of a word occurrence in target_word and adds the second element
+    # (the word usage) to word_count
+    for listing in target_word:
+        word_count += int(listing[1])
+
+    return word_count
